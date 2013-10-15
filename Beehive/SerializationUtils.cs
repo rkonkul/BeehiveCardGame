8e9a2628bea07991ee56f7a832daf0a69b9de3f0
Ryan <rkonkul@gmail.com>
2013-10-14 19:44:07 -0500

    added project

---
+++ Beehive/SerializationUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+
+
+class SerializationUtils
+{
+
+    //string s = SerializationUtils.SerializeObject(g);
+    //BeehiveGame a = (BeehiveGame)SerializationUtils.DeserializeObject(s, typeof(BeehiveGame));
+    public static string SerializeObject(object o)
+    {
+        XmlSerializer xmlSerializer = new XmlSerializer(o.GetType());
+        StringWriter textWriter = new StringWriter();
+        xmlSerializer.Serialize(textWriter, o);
+        return textWriter.ToString();
+    }
+
+    public static object DeserializeObject(string s, Type t)
+    {
+        XmlSerializer mySerializer = new XmlSerializer(t);
+        StringReader reader = new StringReader(s);
+        return mySerializer.Deserialize(reader);
+    }
+}
+
